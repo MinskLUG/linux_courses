878f09183144f52b65e7526007d6e9322e9dbf99
Denis Pynkin <denis_pynkin@epam.com>
2013-03-10 12:16:02 +0300

    Mind Map updated

--- epam/MindMap/Инструменты разработчика.mm
+++ epam/MindMap/Инструменты разработчика.mm
@@ -1,9 +1,10 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1345456886908" ID="ID_1187362394" LINK="LinuxCourses.mm" MODIFIED="1351264467514" TEXT="&#x418;&#x43d;&#x441;&#x442;&#x440;&#x443;&#x43c;&#x435;&#x43d;&#x442;&#x44b; &#x440;&#x430;&#x437;&#x440;&#x430;&#x431;&#x43e;&#x442;&#x447;&#x438;&#x43a;&#x430;.">
+<node CREATED="1345456886908" ID="ID_1187362394" LINK="LinuxCourses.mm" MODIFIED="1362138285170" TEXT="&#x418;&#x43d;&#x441;&#x442;&#x440;&#x443;&#x43c;&#x435;&#x43d;&#x442;&#x44b; &#x440;&#x430;&#x437;&#x440;&#x430;&#x431;&#x43e;&#x442;&#x447;&#x438;&#x43a;&#x430;.">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1350395677533" ID="ID_39996900" MODIFIED="1351264467513" POSITION="right" TEXT="GNU toolchain">
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1361797896693" ID="ID_893602554" MODIFIED="1361797905747" TEXT="&#x43f;&#x440;&#x435;&#x43f;&#x440;&#x43e;&#x446;&#x435;&#x441;&#x441;&#x43e;&#x440;"/>
 <node CREATED="1350398296480" ID="ID_1763256056" MODIFIED="1351264467513" TEXT="&#x43a;&#x43e;&#x43c;&#x43f;&#x438;&#x43b;&#x44f;&#x442;&#x43e;&#x440;">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1351240181013" ID="ID_1353135591" MODIFIED="1351264467513" TEXT="CFLAGS">
@@ -38,60 +39,93 @@
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1350395690615" ID="ID_192248845" MODIFIED="1351264467510" POSITION="right" TEXT="&#x41f;&#x43e;&#x43b;&#x435;&#x437;&#x43d;&#x44b;&#x435; &#x443;&#x442;&#x438;&#x43b;&#x438;&#x442;&#x44b;">
-<font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1350395744631" ID="ID_1907798821" MODIFIED="1351264467510" TEXT="strip">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350396690588" ID="ID_43610111" MODIFIED="1351264467510" TEXT="chroot">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1351241055022" ID="ID_981593207" MODIFIED="1351264467510" TEXT="&#x420;&#x430;&#x431;&#x43e;&#x442;&#x430; &#x441; &#x438;&#x441;&#x445;&#x43e;&#x434;&#x43d;&#x44b;&#x43c; &#x43a;&#x43e;&#x434;&#x43e;&#x43c;">
-<font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1350395759229" ID="ID_1675774642" MODIFIED="1351264467509" TEXT="indent">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350396376401" ID="ID_1032077888" MODIFIED="1351264467509" TEXT="patch">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350396370693" ID="ID_369442792" MODIFIED="1351264467509" TEXT="diff">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1351245554287" ID="ID_1487477173" MODIFIED="1351264467509" TEXT="backtrace()">
-<font NAME="SansSerif" SIZE="12"/>
-<icon BUILTIN="help"/>
-</node>
-</node>
-<node CREATED="1351241097884" ID="ID_503307574" MODIFIED="1351264467508" TEXT="&#x410;&#x43d;&#x430;&#x43b;&#x438;&#x437; &#x438;&#x441;&#x43f;&#x43e;&#x43b;&#x43d;&#x44f;&#x435;&#x43c;&#x43e;&#x433;&#x43e; &#x444;&#x430;&#x439;&#x43b;&#x430;">
-<font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1350395718039" ID="ID_1957190689" MODIFIED="1351264467508" TEXT="ldd">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350395724429" ID="ID_1097692819" MODIFIED="1351264467508" TEXT="objdump">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350395733562" ID="ID_739535204" MODIFIED="1351264467508" TEXT="nm">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350395738149" ID="ID_1135179683" MODIFIED="1351264467507" TEXT="readelf">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350395747854" ID="ID_1731698336" MODIFIED="1351264467507" TEXT="strace">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350395752183" ID="ID_1127492534" MODIFIED="1351264467507" TEXT="ltrace">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-</node>
-</node>
-<node CREATED="1350396227740" ID="ID_1582856650" MODIFIED="1351264467507" POSITION="right" TEXT="&#x421;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x430;&#x432;&#x442;&#x43e;&#x434;&#x43e;&#x43a;&#x443;&#x43c;&#x435;&#x43d;&#x442;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x44f; &#x43a;&#x43e;&#x434;&#x430;">
+<node CREATED="1350396227740" ID="ID_1582856650" MODIFIED="1362138376397" POSITION="right" TEXT="&#x421;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x430;&#x432;&#x442;&#x43e;&#x434;&#x43e;&#x43a;&#x443;&#x43c;&#x435;&#x43d;&#x442;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x44f; &#x43a;&#x43e;&#x434;&#x430;">
 <font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="help"/>
 <node CREATED="1350396244708" ID="ID_1074121466" MODIFIED="1351264467507" TEXT="doxygen">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1351091316255" ID="ID_319568391" MODIFIED="1351264467506" POSITION="right" TEXT="&#x41f;&#x440;&#x43e;&#x444;&#x430;&#x439;&#x43b;&#x438;&#x43d;&#x433;">
+<node CREATED="1350395690615" ID="ID_192248845" MODIFIED="1362574251422" POSITION="right" TEXT="&#x41f;&#x43e;&#x43b;&#x435;&#x437;&#x43d;&#x44b;&#x435; &#x443;&#x442;&#x438;&#x43b;&#x438;&#x442;&#x44b; (binutils)">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1350395718039" ID="ID_1957190689" MODIFIED="1362574223017" TEXT="ldd">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1351241097884" ID="ID_503307574" MODIFIED="1362488814134" TEXT="&#x410;&#x43d;&#x430;&#x43b;&#x438;&#x437; &#x438;&#x441;&#x43f;&#x43e;&#x43b;&#x43d;&#x44f;&#x435;&#x43c;&#x43e;&#x433;&#x43e; &#x444;&#x430;&#x439;&#x43b;&#x430;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1362488868065" ID="ID_1714280908" MODIFIED="1362488875465" TEXT="&#x421;&#x442;&#x440;&#x443;&#x43a;&#x442;&#x443;&#x440;&#x430;"/>
+<node CREATED="1350395738149" ID="ID_1135179683" MODIFIED="1362577023211" TEXT="readelf">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1350395724429" ID="ID_1097692819" MODIFIED="1362488835596" TEXT="objdump">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1350395733562" ID="ID_739535204" MODIFIED="1351264467508" TEXT="nm">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1362577036304" ID="ID_1440354957" MODIFIED="1362577050706" TEXT="&#x421;&#x435;&#x43a;&#x446;&#x438;&#x44f; &#x441;&#x438;&#x43c;&#x432;&#x43e;&#x43b;&#x43e;&#x432;">
+<node CREATED="1362138695248" ID="ID_1062773388" MODIFIED="1362577060345" TEXT="strings"/>
+<node CREATED="1350395744631" ID="ID_1907798821" MODIFIED="1362577054879" TEXT="strip">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1362138285156" ID="ID_1300287314" MODIFIED="1362138367513" POSITION="right" TEXT="&#x421;&#x43f;&#x43e;&#x441;&#x43e;&#x431;&#x44b; &#x438;&#x437;&#x43e;&#x43b;&#x44f;&#x446;&#x438;&#x438; &#x43f;&#x440;&#x43e;&#x446;&#x435;&#x441;&#x441;&#x430;">
+<icon BUILTIN="help"/>
+<node CREATED="1350396690588" ID="ID_43610111" MODIFIED="1351264467510" TEXT="chroot">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1362138308389" ID="ID_303836648" MODIFIED="1362138315087" TEXT="lxc"/>
+<node CREATED="1362138321382" ID="ID_1406256499" MODIFIED="1362138324648" TEXT="cgroups"/>
+</node>
+<node CREATED="1350396256988" ID="ID_183303167" MODIFIED="1351264467504" POSITION="right" TEXT="&#x41e;&#x442;&#x43b;&#x430;&#x434;&#x43a;&#x430;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1361798032912" ID="ID_1986926386" MODIFIED="1361798043718" TEXT="printf"/>
+<node CREATED="1362489303840" ID="ID_942316479" MODIFIED="1362489316537" TEXT="&#x422;&#x440;&#x430;&#x441;&#x441;&#x438;&#x440;&#x43e;&#x432;&#x43a;&#x430;">
+<node CREATED="1350395747854" ID="ID_1731698336" MODIFIED="1362138533259" TEXT="strace">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1350395752183" ID="ID_1127492534" MODIFIED="1362138533263" TEXT="ltrace">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1362488742661" ID="ID_894585896" MODIFIED="1362488745192" TEXT="ptrace"/>
+<node CREATED="1362489324607" ID="ID_1248026964" MODIFIED="1362489392844" TEXT="Stack trace">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      http://blog.bigpixel.ro/2010/09/stack-unwinding-stack-trace-with-gcc/
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1351245554287" ID="ID_1487477173" MODIFIED="1362489374472" TEXT="backtrace()">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1362489411763" ID="ID_1888232486" MODIFIED="1362489415647" TEXT="backtrace_symbols()"/>
+<node CREATED="1362489458370" ID="ID_1345239275" MODIFIED="1362906757097" TEXT="libunwind">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1350396389031" ID="ID_132226597" MODIFIED="1351264467504" TEXT="gdb">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1362578740713" ID="ID_1199714303" MODIFIED="1362578753103" TEXT="&#x41e;&#x442;&#x43b;&#x430;&#x434;&#x43e;&#x447;&#x43d;&#x44b;&#x435; &#x441;&#x438;&#x43c;&#x432;&#x43e;&#x43b;&#x44b;"/>
+<node CREATED="1362578698471" ID="ID_517952183" MODIFIED="1362578737380" TEXT=".dbg &#x444;&#x430;&#x439;&#x43b;&#x44b; (objdump)"/>
+<node CREATED="1351245482759" ID="ID_1684951704" MODIFIED="1362138803710" TEXT="core dump">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1350396396468" ID="ID_807709019" MODIFIED="1351264467504" TEXT="valgrind">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1351095568667" ID="ID_1043966428" MODIFIED="1351264467504" TEXT="kcachegrind">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1351091316255" ID="ID_319568391" MODIFIED="1361797977231" POSITION="right" TEXT="&#x41f;&#x440;&#x43e;&#x444;&#x430;&#x439;&#x43b;&#x438;&#x43d;&#x433;">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1351245058362" ID="ID_1502248309" MODIFIED="1351264467506" TEXT="&#x41e;&#x43f;&#x440;&#x435;&#x434;&#x435;&#x43b;&#x435;&#x43d;&#x438;&#x435; &#x443;&#x437;&#x43a;&#x438;&#x445; &#x43c;&#x435;&#x441;&#x442;">
 <font NAME="SansSerif" SIZE="12"/>
@@ -105,26 +139,25 @@
 <node CREATED="1351243312655" ID="ID_630030939" MODIFIED="1351264467505" TEXT="vmstat">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-</node>
-<node CREATED="1350396256988" ID="ID_183303167" MODIFIED="1351264467504" POSITION="right" TEXT="&#x41e;&#x442;&#x43b;&#x430;&#x434;&#x43a;&#x430;">
-<font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1350396389031" ID="ID_132226597" MODIFIED="1351264467504" TEXT="gdb">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1350396396468" ID="ID_807709019" MODIFIED="1351264467504" TEXT="valgrind">
-<font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1351095568667" ID="ID_1043966428" MODIFIED="1351264467504" TEXT="kcachegrind">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1351245482759" ID="ID_1684951704" MODIFIED="1351264467503" TEXT="core dump">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
+<node CREATED="1362138549377" ID="ID_801596972" MODIFIED="1362138553120" TEXT="systemtap"/>
 </node>
 <node CREATED="1350396754739" ID="ID_520487049" MODIFIED="1351264467503" POSITION="right" TEXT="&#x421;&#x43e;&#x432;&#x43c;&#x435;&#x441;&#x442;&#x43d;&#x430;&#x44f; &#x440;&#x430;&#x437;&#x440;&#x430;&#x431;&#x43e;&#x442;&#x43a;&#x430;">
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1351241055022" ID="ID_981593207" MODIFIED="1362138738857" TEXT="&#x420;&#x430;&#x431;&#x43e;&#x442;&#x430; &#x441; &#x438;&#x441;&#x445;&#x43e;&#x434;&#x43d;&#x44b;&#x43c; &#x43a;&#x43e;&#x434;&#x43e;&#x43c;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1350395759229" ID="ID_1675774642" MODIFIED="1351264467509" TEXT="indent">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1350396376401" ID="ID_1032077888" MODIFIED="1351264467509" TEXT="patch">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1350396370693" ID="ID_369442792" MODIFIED="1351264467509" TEXT="diff">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
 <node CREATED="1350396788903" ID="ID_841086715" MODIFIED="1351264467503" TEXT="&#x421;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x43a;&#x43e;&#x43d;&#x442;&#x440;&#x43e;&#x43b;&#x44f; &#x432;&#x435;&#x440;&#x441;&#x438;&#x439;">
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1362138825559" ID="ID_296610652" MODIFIED="1362138832342" TEXT="Best practices"/>
 <node CREATED="1350397796462" ID="ID_282084815" MODIFIED="1351264467503" TEXT="subversion">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
@@ -137,6 +170,18 @@
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1351004378153" ID="ID_1876702973" MODIFIED="1351264467502" TEXT="&#x423;&#x441;&#x442;&#x430;&#x43d;&#x43e;&#x432;&#x43a;&#x430; &#x438;&#x437; &#x438;&#x441;&#x445;&#x43e;&#x434;&#x43d;&#x438;&#x43a;&#x43e;&#x432;">
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1362138853493" ID="ID_1403093074" MODIFIED="1362138864198" TEXT="&#x427;&#x435;&#x43c; &#x44d;&#x442;&#x43e; &#x43f;&#x43b;&#x43e;&#x445;&#x43e;"/>
+<node CREATED="1362138876093" ID="ID_317631544" MODIFIED="1362138973570" TEXT="&#x421;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x443;&#x43f;&#x440;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x438;&#x44f; &#x43f;&#x430;&#x43a;&#x435;&#x442;&#x430;&#x43c;&#x438; &#x434;&#x43b;&#x44f; &#x44f;&#x437;&#x44b;&#x43a;&#x43e;&#x432;">
+<node CREATED="1362138893251" ID="ID_53264048" MODIFIED="1362138895821" TEXT="ruby">
+<node CREATED="1362138942955" ID="ID_695005144" MODIFIED="1362138945939" TEXT="rubygem"/>
+</node>
+<node CREATED="1362138898781" ID="ID_350548269" MODIFIED="1362138901796" TEXT="perl">
+<node CREATED="1362138903147" ID="ID_1898351481" MODIFIED="1362138909482" TEXT="CPAN"/>
+</node>
+<node CREATED="1362138915047" ID="ID_1847601511" MODIFIED="1362138921182" TEXT="java">
+<node CREATED="1362138923457" ID="ID_430299237" MODIFIED="1362138925706" TEXT="maven"/>
+</node>
+</node>
 </node>
 <node CREATED="1350396411962" ID="ID_395153412" MODIFIED="1351264467502" TEXT="&#x41f;&#x430;&#x43a;&#x435;&#x442;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435;">
 <font NAME="SansSerif" SIZE="12"/>
