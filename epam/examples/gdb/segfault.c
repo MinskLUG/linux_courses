709d49e74d9e55acf186909d54d6b9ed35c850cb
begemotv2718 <begemotv2718@gmail.com>
2013-03-11 14:45:34 +0300

    segfault program

---
+++ epam/examples/gdb/segfault.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 1000
+
+void fill_random_ascii_buffer(char *buffer,size_t size){
+  int i;
+  for(i=0;i<size;i++){
+    buffer[i]=32+(int)(drand48()*(127.0-32.0));
+  }
+}
+
+int main(int argc, char *argv[]){
+  char *buf;
+  int j=0;
+  buf=malloc(BUF_SIZE*sizeof(char));
+  if(buf=NULL){
+    printf("Buffer allocation failed!\n");
+    exit(1);
+  }
+
+  fill_random_ascii_buffer(buf,BUF_SIZE);
+
+  for(j=0;j<BUF_SIZE;j++){
+    putchar(buf[j]);
+    if(j%50){
+      putchar('\n');
+    }
+  }
+  putchar('\n');
+  return(0);
+}
+
