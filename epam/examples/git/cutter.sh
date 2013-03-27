4c65ca9e92cbb2cb6548f283d3e30f38341cf47d
Denis Pynkin <denis_pynkin@epam.com>
2013-03-27 16:15:42 +0300

    Added cutter

---
+++ epam/examples/git/cutter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+
+# Script for git exercises
+# 
+
+OUT=text.txt
+
+CUT=$((RANDOM % 100))
+CNT=0
+CRAPFLAG=0
+NUM=0
+
+git init
+touch $OUT
+git add $OUT
+
+git commit $OUT -m "Initial commit" &>/dev/null
+
+exec > $OUT
+while read string
+do
+	if [ $CNT == $CUT ]
+	then
+		git commit -a -m "$NUM part added" &>/dev/null
+		CUT=$((RANDOM % 100))
+		((NUM++))
+		CNT=0
+	fi
+	((CNT++))
+	echo "$string"
+
+	if [ $CRAPFLAG != 1 -a $((RANDOM % 1000)) == 0 -a $NUM -gt 3 ]
+	then
+		CRAPFLAG=1
+		echo "Some anonymous crap from nobody :-E~~~"
+	fi
+
+done
+
+git commit -a -m "Last commit" &>/dev/null
+
