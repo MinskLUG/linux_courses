c2605a8f5afd5b0fe686a1fb87410ab7c5b0fb78
Denis Pynkin <dans@altlinux.org>
2016-01-26 18:00:48 +0300

    Modified linking example
    Added init/fini functions.
    Added linking with --as-needed flag to show difference in loading.

--- epam/examples/make/linking/libtestb.c
+++ epam/examples/make/linking/libtestb.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "libtest.h"
 
-void test(void)
-{
+void test(void){
     printf("Lib B used\n" );
+}
+
+void init(void){
+    printf("Lib B loaded\n" );
+}
+
+void fini(void){
+    printf("Lib B unloaded\n" );
 }
